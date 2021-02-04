610def2f2e994a925ded2eab39da633e593610f1
sonu <skb3182@gmail.com>
2021-02-04 18:14:06 +0530

    Second Commit

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,16 @@
-def main():
+
+
+
+    def printBoard():
+        # "board" is a list of 10 strings representing the board (ignore index 0)
+        print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+        print('-----------')
+        print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+        print('-----------')
+        print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+ 
+    def main():
         #Main game loop
         print("Welcome to Tic Tac Toe, to win complete a straight line of your letter (Diagonal, Horizontal, Vertical). The board has positions 1-9 starting at the top left.")
         printBoard()
+    
