0f06afa7a7be184c371121536f4a8b3b2ab279f7
sonu3182 <76956208+sonu3182@users.noreply.github.com>
2021-02-04 18:09:17 +0530

    Create TicTacToe.py

---
+++ TicTacToe.py
@@ -0,0 +1,4 @@
+def main():
+        #Main game loop
+        print("Welcome to Tic Tac Toe, to win complete a straight line of your letter (Diagonal, Horizontal, Vertical). The board has positions 1-9 starting at the top left.")
+        printBoard()
